1085ebf0009d5a8b9f7d757df63fd5b2e7c353a8
xunmao <xunmao1989@gmail.com>
2023-06-04 14:00:18 +0800

    添加新的 Todo 事项

--- src/App.jsx
+++ src/App.jsx
@@ -15,12 +15,29 @@ export default class App extends Component {
     ]
   }
 
+  // 新增 todo 对象，将这个函数传给 Header 组件，即可获取 Header 组件中的数据
+  addTodo = (todoObj) => {
+    // 获取当前的 todo 列表
+    const { todos } = this.state
+    // 新建 todo 列表，将新增的 todo 对象放入第一位，之后放入当前的 todo 列表
+    const newTodos = [todoObj, ...todos]
+    // 用新建的 todo 列表，替换当前的 todo 列表，完成 state 更新
+    this.setState({ todos: newTodos })
+  }
+
   render() {
     const { todos } = this.state
     return (
       <div className='todo-container'>
         <div className='todo-wrap'>
-          <Header />
+          {/* 
+            要想获得 Header 组件中的数据，可以传递一个函数给它，
+            然后，Header 在适当的时机调用传入的函数即可。
+          */}
+          <Header addTodo={this.addTodo} />
+          {/* 
+            要想让父组件传递数据给子组件，可以使用 props 传递。
+          */}
           <List todos={todos} />
           <Footer />
         </div>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,37 @@
 import React, { Component } from 'react'
+import { nanoid } from 'nanoid'
 import './Header.css'
 
 export default class Header extends Component {
+
+  handleKeyUp = (event) => {
+
+    // 0. 从 event 对象中获取 target 和 keyCode
+    const { target, keyCode } = event
+    // 1. 如果用户尚未按下回车键，表示输入尚未完成，跳过后续处理
+    if (keyCode !== 13) { return }
+
+    // 2. 如果用户按下回车键，表示输入已经完成，开始后续处理
+    // 2.1. 如果用户输入的内容为空，需要提示用户重新输入，并跳过后续处理
+    if (target.value.trim() === '') {
+      alert('任务名称不能为空，请重新输入任务名称')
+      return
+    }
+    // 2.2. 如果用户输入的内容不为空，则新建 todo 对象，并传递给 App 组件，以便更新 todo 列表
+    // 2.2.1. 获取 UUID 作为 todo 对象的 id
+    const id = nanoid()
+    // 2.2.2. 使用用户输入的内容作为 todo 对象的 title，isDone 默认设置为 false（表示该 todo 尚未完成）
+    const todo = { id: id, title: target.value, isDone: false }
+    // 2.2.3. 将新建的 todo 对象传递给 App 组件
+    this.props.addTodo(todo)
+    // 2.2.4. 将输入框中的内容清空，以便用户输入新的内容
+    target.value = ''
+  }
+
   render() {
     return (
       <div className='todo-header'>
-        <input type="text" placeholder='请输入你的任务名称，按回车键确认' />
+        <input type="text" placeholder='请输入你的任务名称，按回车键确认' onKeyUp={this.handleKeyUp} />
       </div>
     )
   }
