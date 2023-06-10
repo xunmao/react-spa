f8e14177ed9a77f669aa2663054513086398c144
xunmao <xunmao1989@gmail.com>
2023-06-10 21:03:34 +0800

    添加鼠标移入效果

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -2,15 +2,29 @@ import React, { Component } from 'react'
 import './Item.css'
 
 export default class Item extends Component {
+
+  state = {
+    isMouseHover: false
+  }
+
+  handleMouseEnter = () => {
+    this.setState({ isMouseHover: true })
+  }
+
+  handleMouseLeave = () => {
+    this.setState({ isMouseHover: false })
+  }
+
   render() {
     const { title, isDone } = this.props.data
+    const { isMouseHover } = this.state
     return (
-      <li>
+      <li onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave} >
         <label>
           <input type="checkbox" defaultChecked={isDone} />
           <span>{title}</span>
         </label>
-        <button className='btn btn-danger' style={{ display: 'none' }}>删除</button>
+        <button className='btn btn-danger' style={{ display: isMouseHover ? 'block' : 'none' }}>删除</button>
       </li>
     )
   }
