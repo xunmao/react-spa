fc2d9b7790fc67b61ff3128fe76b7e1d34917380
xunmao <xunmao1989@gmail.com>
2023-06-24 23:04:30 +0800

    GitHub搜索案例 - 静态组件

---
+++ src/components/List/index.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+
+export default class List extends Component {
+  render() {
+    return (
+      <div className="row row-cols-3">
+        <div className='col' >
+          <div className="card">
+            <a href="https://github,com/reactjs" target="_blank" rel='noreferrer'>
+              <img alt='an avatar of a github user' src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{ width: '100px' }} />
+            </a>
+            <p className="card-text">reactjs</p>
+          </div>
+        </div>
+        <div className='col' >
+          <div className="card">
+            <a href="https://github,com/reactjs" target="_blank" rel='noreferrer'>
+              <img alt='an avatar of a github user' src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{ width: '100px' }} />
+            </a>
+            <p className="card-text">reactjs</p>
+          </div>
+        </div>
+        <div className='col' >
+          <div className="card">
+            <a href="https://github,com/reactjs" target="_blank" rel='noreferrer'>
+              <img alt='an avatar of a github user' src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{ width: '100px' }} />
+            </a>
+            <p className="card-text">reactjs</p>
+          </div>
+        </div>
+        <div className='col' >
+          <div className="card">
+            <a href="https://github,com/reactjs" target="_blank" rel='noreferrer'>
+              <img alt='an avatar of a github user' src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{ width: '100px' }} />
+            </a>
+            <p className="card-text">reactjs</p>
+          </div>
+        </div>
+        <div className='col' >
+          <div className="card">
+            <a href="https://github,com/reactjs" target="_blank" rel='noreferrer'>
+              <img alt='an avatar of a github user' src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{ width: '100px' }} />
+            </a>
+            <p className="card-text">reactjs</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
